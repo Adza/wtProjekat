041f6e57ac1c00b8a8a9c489f53d85e77202eb6c
Armin Omerbegovic <aomerbegov2@etf.unsa.ba>
2018-01-22 21:49:08 +0100

    port2

--- baza.js
+++ baza.js
@@ -1,3 +1,3 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("baza","wtspirala","wtspirala",{host:"mysql-57-centos7",dialect:"mysql",port:3360});
+const sequelize = new Sequelize("baza","wtspirala","wtspirala",{host:"mysql-57-centos7",dialect:"mysql",port:3306});
 module.exports = sequelize;
